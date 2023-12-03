d51056675b3f4e54df7d5aef6c792d109a831d9f
Levi Lesches <levilesches@gmail.com>
2023-12-03 16:21:37 -0500

    Updated Protobuf

--- lib/src/generated/logs.pbjson.dart
+++ lib/src/generated/logs.pbjson.dart
@@ -33,38 +33,6 @@ final $typed_data.Uint8List burtLogLevelDescriptor = $convert.base64Decode(
     'wQARIJCgVlcnJvchACEgsKB3dhcm5pbmcQAxIICgRpbmZvEAQSCQoFZGVidWcQBRIJCgV0cmFj'
     'ZRAG');
 
-@$core.Deprecated('Use burtErrorDescriptor instead')
-const BurtError$json = {
-  '1': 'BurtError',
-  '2': [
-    {'1': 'burt_error_undefined', '2': 0},
-    {'1': 'burt_error_unknown', '2': 1},
-    {'1': 'can_init_error', '2': 2},
-    {'1': 'can_write_error', '2': 3},
-    {'1': 'can_read_error', '2': 4},
-    {'1': 'can_buffer_full', '2': 5},
-    {'1': 'can_misc_error', '2': 6},
-    {'1': 'imu_init_error', '2': 7},
-    {'1': 'imu_not_responding', '2': 8},
-    {'1': 'imu_invalid_orientation', '2': 9},
-    {'1': 'imu_misc_error', '2': 10},
-    {'1': 'gps_init_error', '2': 11},
-    {'1': 'gps_not_responding', '2': 12},
-    {'1': 'gps_no_fix', '2': 13},
-    {'1': 'gps_misc_error', '2': 14},
-  ],
-};
-
-/// Descriptor for `BurtError`. Decode as a `google.protobuf.EnumDescriptorProto`.
-final $typed_data.Uint8List burtErrorDescriptor = $convert.base64Decode(
-    'CglCdXJ0RXJyb3ISGAoUYnVydF9lcnJvcl91bmRlZmluZWQQABIWChJidXJ0X2Vycm9yX3Vua2'
-    '5vd24QARISCg5jYW5faW5pdF9lcnJvchACEhMKD2Nhbl93cml0ZV9lcnJvchADEhIKDmNhbl9y'
-    'ZWFkX2Vycm9yEAQSEwoPY2FuX2J1ZmZlcl9mdWxsEAUSEgoOY2FuX21pc2NfZXJyb3IQBhISCg'
-    '5pbXVfaW5pdF9lcnJvchAHEhYKEmltdV9ub3RfcmVzcG9uZGluZxAIEhsKF2ltdV9pbnZhbGlk'
-    'X29yaWVudGF0aW9uEAkSEgoOaW11X21pc2NfZXJyb3IQChISCg5ncHNfaW5pdF9lcnJvchALEh'
-    'YKEmdwc19ub3RfcmVzcG9uZGluZxAMEg4KCmdwc19ub19maXgQDRISCg5ncHNfbWlzY19lcnJv'
-    'chAO');
-
 @$core.Deprecated('Use burtLogDescriptor instead')
 const BurtLog$json = {
   '1': 'BurtLog',
