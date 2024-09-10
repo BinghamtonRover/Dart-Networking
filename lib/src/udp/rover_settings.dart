78cd0fc8c922cf62b054653c54eb448ba866ba8a
Levi Lesches <levilesches@gmail.com>
2024-09-10 04:07:45 -0400

    Skip sudo shutdown now in CI tests

--- lib/src/udp/rover_settings.dart
+++ lib/src/udp/rover_settings.dart
@@ -19,7 +19,7 @@ mixin RoverSettings on BurtSocket {
       } catch (error) {
         logger.critical("Error when shutting down: $error");
       }
-      if (!Platform.isLinux) return;
+      if (!Platform.isLinux || Platform.environment["IS_TEST"] != null) return;
       await Process.run("sudo", ["shutdown", "now"]);
     } else if (settings.status == RoverStatus.RESTART) {
       await restart();
