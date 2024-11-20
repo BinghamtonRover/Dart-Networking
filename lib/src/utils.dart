49cbd841af01ab0ada5c0cdf01bbcdd55d7a9893
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-11-20 13:55:29 -0500

    Add utility methods for GPS units conversion

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,4 +1,5 @@
 import "dart:async";
+import "dart:math";
 
 import "package:burt_network/generated.dart";
 
@@ -44,3 +45,30 @@ String? deviceToDataName(Device device) => switch (device) {
   Device.SCIENCE => ScienceData().messageName,
   _ => null,
 };
+
+/// Helpful extension methods to convert GpsCoordinates into meters
+extension GpsCoordinatesToMeters on GpsCoordinates {
+  /// Number of meters per degree of latitude
+  static const metersPerLatitude = 111.32 * 1000;
+
+  /// Converts GpsCoordinates into meters
+  (num longitudeMeters, num latitudeMeters) get inMeters {
+    final metersPerLongitude = 40075 * cos(latitude * (pi / 180)) / 360 * 1000;
+    return (latitude * metersPerLatitude, longitude * metersPerLongitude);
+  }
+}
+
+/// Extension to convert coordinates in meters into Gps Coordinates
+extension MeterRecordToGps on (num, num) {
+  /// Convert a record of 2 doubles (which represent longitude and latitude coordinates) into Gps Coordinates
+  GpsCoordinates get toGpsCoordinates {
+    final latitude = $1 / GpsCoordinatesToMeters.metersPerLatitude;
+    final metersPerLongitude = 40075 * cos(latitude * (pi / 180)) / 360 * 1000;
+    return GpsCoordinates(latitude: latitude, longitude: $2 / metersPerLongitude);
+  }
+
+  /// Convert a record of 2 doubles (which represent longitude and latitude coordinates) into Gps Coordinates
+  /// 
+  /// Alias for [MeterRecordToGps.toGpsCoordinates]
+  GpsCoordinates toGps() => toGpsCoordinates;
+}
