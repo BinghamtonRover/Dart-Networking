91055c2103b8203c5dfaac315720d7c68e12a3ae
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-11-21 19:56:46 -0500

    Added utility method to exchange a handshake

--- lib/src/udp/burt_socket.dart
+++ lib/src/udp/burt_socket.dart
@@ -77,6 +77,37 @@ abstract class BurtSocket extends UdpSocket {
     await super.dispose();
   }
 
+  /// A utility method to exchange a "handshake" to the destination
+  /// 
+  /// This will immediately send the [message], and will complete once the
+  /// message is received again by this socket. If the message is not received
+  /// after [timeout] amount of time, then it will complete false
+  /// 
+  /// This assumes that there is a destination that is expecting this
+  /// message and will immediately echo it back once receiving it
+  Future<bool> tryHandshake<T extends Message>({
+    required T message,
+    required Duration timeout,
+    required String messageName,
+    required T Function(List<int> bytes) constructor,
+  }) {
+    sendMessage(message);
+    final completer = Completer<bool>();
+
+    late StreamSubscription<T> subscription;
+
+    subscription = messages.onMessage(
+      name: messageName,
+      constructor: constructor,
+      callback: (handshake) {
+        completer.complete(true);
+        subscription.cancel();
+      },
+    );
+
+    return completer.future.timeout(timeout, onTimeout: () => false);
+  }
+
   void _onPacket(Datagram packet) {
     final wrapper = packet.parseWrapper();
     if (wrapper.name == Connect().messageName) {
